1034f30bdd3757332c1830221b918706b3baab0a
Raaj Anand Chaudhary <raaj.anand2@gmail.com>
2020-09-13 18:09:37 +0530

    'Sign in' and 'Register' forms working
    Signed-off-by: Raaj Anand Chaudhary <raaj.anand2@gmail.com>

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -9,29 +9,50 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
 
+  //text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.teal,
       appBar: AppBar(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal[600],
         title: Text("Sign In"),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: RaisedButton(
-          child: Text('Sign In Anon'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result.uid);
-            }
-          },
-        ),
-      ),
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          child: Form(
+              child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                color: Colors.teal[600],
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                },
+              )
+            ],
+          ))),
     );
   }
 }
